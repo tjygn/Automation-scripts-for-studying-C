e86290b8b081302d7aafa92bf9eba25a85e83699
Sergey MintLab <sergey110208@mail.ru>
2025-11-21 19:33:22 +0300

    Set the timer to 50 minutes, updated README

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ open 'C Programming - A Modern Approach - 2nd_Ed(C89, c99) - King by .pdf'
 xdg-open http://knking.com/books/c2/answers/index.html
 time_didnt_elapse="1"
 while [ $time_didnt_elapse="1" ] ; do
-	if [ $SECONDS = "10" ]; then
+	if [ $SECONDS = "3000" ]; then
 		echo "Time to take a break!"
 		time_didnt_elapse="0"
 		break
