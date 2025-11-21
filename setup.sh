4c76235cba44f07d667c703910a381b858957a3c
Sergey MintLab <sergey110208@mail.ru>
2025-11-21 18:55:37 +0300

    Added missing symbols

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ cd Documents/
 open 'C Programming - A Modern Approach - 2nd_Ed(C89, c99) - King by .pdf'
 xdg-open http://knking.com/books/c2/answers/index.html
 time_didnt_elapse = 1
-while [ time_didnt_elapse = 1 ; do
-	if [ SECONDS = 10 ]; then
+while [ time_didnt_elapse = 1 ] ; do
+	if [ $SECONDS = 10 ]; then
 		echo "Time to take a break!"
-		time_didnt_elapse = 0
+		$time_didnt_elapse = 0
 	fi
 done
 
