2e67eff241cd0e4c114016b02b899b4bfb4da88c
Sergey MintLab <sergey110208@mail.ru>
2025-11-21 18:59:50 +0300

    Removed spaces in variable assignment

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-SECONDS = 0
+SECONDS=0
 subl
 cd
 cd Documents/
 open 'C Programming - A Modern Approach - 2nd_Ed(C89, c99) - King by .pdf'
 xdg-open http://knking.com/books/c2/answers/index.html
-time_didnt_elapse = 1
+time_didnt_elapse=1
 while [ time_didnt_elapse = 1 ] ; do
 	if [ $SECONDS = 10 ]; then
 		echo "Time to take a break!"
