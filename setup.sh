a3f42c343c7be47d9189a53c92eca8093d9fe471
Sergey MintLab <sergey110208@mail.ru>
2025-11-21 19:03:57 +0300

    Added kavichky in seconds variable

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SECONDS=0
+SECONDS="0"
 subl
 cd
 cd Documents/
@@ -7,7 +7,7 @@ open 'C Programming - A Modern Approach - 2nd_Ed(C89, c99) - King by .pdf'
 xdg-open http://knking.com/books/c2/answers/index.html
 time_didnt_elapse=1
 while [ time_didnt_elapse = 1 ] ; do
-	if [ $SECONDS = 10 ]; then
+	if [ $SECONDS = "10" ]; then
 		echo "Time to take a break!"
 		$time_didnt_elapse = 0
 	fi
